a4223c6c3812d95d6386a7cdd161d1cc96537fb2
Izeytee <I-zey@yandex.ru>
2018-05-13 20:03:00 +0600

    Create GenerateIrregularVerb

---
+++ src/GenerateIrregularVerb.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+GenerateIrregularVerb(char GeneratedWord[4][30])
+{
+   srand(time(0));
+   int GeneratedString = rand()%5, StringNumber = 0, WordNumber = 0;
+   int LetterNumber = 0;
+   char ch;
+   for (int i = 0; i < 4; i++)
+   {
+   	  for (int j = 0; j < 30; j++)
+   	  GeneratedWord[i][j] = ' ';
+   }
+   FILE *SearchVerb;
+   SearchVerb=fopen("IrregularVerbs.txt","r");
+   while(1)
+   {
+   	  while(StringNumber != GeneratedString)
+      {
+       	 ch = getc(SearchVerb);
+         if (ch == '\n')
+         StringNumber++;
+      }
+      while(WordNumber != 3)
+      {
+         ch = getc(SearchVerb);
+         if (ch == ' ')
+         {
+            WordNumber++;
+            LetterNumber = 0;
+		 }
+		 else
+		 {
+		    GeneratedWord[WordNumber][LetterNumber] = ch;
+		    LetterNumber++;
+		 }
+	  }
+	  LetterNumber = 0;
+	  while(ch != '\n')
+	  {
+	     ch = getc(SearchVerb);
+		 GeneratedWord[WordNumber][LetterNumber] = ch;
+		 LetterNumber++;
+	  }
+	break;
+	}
+   fclose(SearchVerb);
+}
